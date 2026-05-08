4a50c248696b840371f96169fe9b552b2c4a901a
2002shirley <1085067@senacminas.edu.br>
2026-05-07 21:55:18 -0300

    finalização do interface grafica

--- src/InterfaceGrafica.java
+++ src/InterfaceGrafica.java
@@ -1,3 +1,6 @@
+
+import javax.swing.JOptionPane;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
@@ -25,13 +28,19 @@ public class InterfaceGrafica extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jComboBox1 = new javax.swing.JComboBox<>();
+        jComboBox2 = new javax.swing.JComboBox<>();
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         txfPeso = new javax.swing.JTextField();
         cbPlanetas = new javax.swing.JComboBox<>();
         jLabel3 = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
+        jbCalcularPeso = new javax.swing.JButton();
+
+        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+
+        jComboBox2.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -49,33 +58,36 @@ public class InterfaceGrafica extends javax.swing.JFrame {
             }
         });
 
+        jLabel3.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         jLabel3.setText("Planeta Desejado :");
 
-        jButton1.setText("jButton1");
+        jbCalcularPeso.setText("Calcular Peso ");
+        jbCalcularPeso.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jbCalcularPesoActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(125, 125, 125)
+                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 127, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                    .addComponent(cbPlanetas, javax.swing.GroupLayout.Alignment.LEADING, 0, 120, Short.MAX_VALUE)
-                    .addComponent(txfPeso, javax.swing.GroupLayout.Alignment.LEADING))
-                .addGap(141, 141, 141))
-            .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(152, 152, 152)
-                        .addComponent(jButton1))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(125, 125, 125)
-                        .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addComponent(jbCalcularPeso)
+                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                        .addComponent(cbPlanetas, javax.swing.GroupLayout.Alignment.LEADING, 0, 120, Short.MAX_VALUE)
+                        .addComponent(txfPeso, javax.swing.GroupLayout.Alignment.LEADING)))
+                .addGap(141, 141, 141))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -91,7 +103,7 @@ public class InterfaceGrafica extends javax.swing.JFrame {
                     .addComponent(jLabel1)
                     .addComponent(txfPeso, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 31, Short.MAX_VALUE)
-                .addComponent(jButton1)
+                .addComponent(jbCalcularPeso)
                 .addGap(27, 27, 27))
         );
 
@@ -113,14 +125,70 @@ public class InterfaceGrafica extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void cbPlanetasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cbPlanetasActionPerformed
-        // TODO add your handling code here:
+     
     }//GEN-LAST:event_cbPlanetasActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
+    private void jbCalcularPesoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbCalcularPesoActionPerformed
+        
+        try {
+            String planetaSelecionado = cbPlanetas.getSelectedItem().toString();
+            double peso = Double.parseDouble(txfPeso.getText());
+            double gravidade = 0;
+
+        
+            switch (planetaSelecionado) {
+
+                case "Mercúrio":
+                    gravidade = 3.6;
+                    break;
+
+                case "Vênus":
+                    gravidade = 8.7;
+                    break;
+
+                case "Terra":
+                    JOptionPane.showMessageDialog(null, 
+                            "O seu peso na Terra é "+txfPeso.getText());
+                    return;
+                    
+
+                case "Marte":
+                    gravidade = 3.7;
+                    break;
+
+                case "Júpiter":
+                    gravidade = 24.79;
+                    break;
+
+                case "Saturno":
+                    gravidade = 10.44;
+                    break;
+
+                case "Urano":
+                    gravidade = 8.69;
+                    break;
+
+                case "Netuno":
+                    gravidade = 11.15;
+                    break;
+
+                case "Lua":
+                    gravidade = 1.6;
+                    break;
+            }
+            
+            String mensagem = "";
+            mensagem += "O seu peso no "+planetaSelecionado+" ";
+            mensagem += peso / 10 * gravidade;
+            
+            JOptionPane.showMessageDialog(null, mensagem);
+
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Erro,Campo não pode está vazio!");
+        }
+    }//GEN-LAST:event_jbCalcularPesoActionPerformed
+
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
@@ -143,7 +211,6 @@ public class InterfaceGrafica extends javax.swing.JFrame {
         }
         //</editor-fold>
 
-        /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new InterfaceGrafica().setVisible(true);
@@ -153,11 +220,13 @@ public class InterfaceGrafica extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JComboBox<String> cbPlanetas;
-    private javax.swing.JButton jButton1;
+    private javax.swing.JComboBox<String> jComboBox1;
+    private javax.swing.JComboBox<String> jComboBox2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JButton jbCalcularPeso;
     private javax.swing.JTextField txfPeso;
     // End of variables declaration//GEN-END:variables
 }
